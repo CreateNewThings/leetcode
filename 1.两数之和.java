08f26eb2b13da57ca429d1a5447bb8d7667a3fc8
CreateIt <2323598952@qq.com>
2022-05-25 17:50:02 +0800

    first commit

---
+++ 1.两数之和.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
